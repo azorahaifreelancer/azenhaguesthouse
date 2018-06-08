d5549d343adb90c452551f267ddd3de69fb89809
azorahaifreelancer <azorahaifreelancer@gmail.com>
2018-06-08 17:42:26 +0100

    removendo favicon folder

--- book.php
+++ book.php
@@ -8,22 +8,22 @@
   //form is submitted with POST method
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["name"])) {
-      $name_error = "Name is required";
+      $name_error = "Preencha com o seu nome";
     } else {
       $name = test_input($_POST["name"]);
       // check if name only contains letters and whitespace
       if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-        $name_error = "Only letters and white space allowed";
+        $name_error = "Apenas letras e espaços são permitidos";
       }
     }
 
     if (empty($_POST["email"])) {
-      $email_error = "Email is required";
+      $email_error = "Preencha com o seu nome email";
     } else {
       $email = test_input($_POST["email"]);
       // check if e-mail address is well-formed
       if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $email_error = "Invalid email format";
+        $email_error = "Formato inválido";
       }
     }
 
@@ -47,9 +47,9 @@
         }
 
         $to = 'info@azenhaguesthouse.com';
-        $subject = 'Contact Form Submit';
+        $subject = 'Azenha GuestHouse';
         if (mail($to, $subject, $message)){
-            $success = "Message sent, thank you for contacting us!";
+            $success = "Mensagem enviada, obrigado pelo contacto!";
             $name = $email = $phone = $message = '';
         }
     }
--- header.php
+++ header.php
@@ -5,49 +5,31 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <title>Azenha GuestHouse | Turismo Rural</title>
 <meta name="keywords" content="guesthouse, hotel, alojamento, turismo, turismo rural, estadia, ferias, quarto, quartos,  foz do arelho, portugal, obidos, caldas da rainha, leiria, lagoa de obidos, penedo furado, costa de prata" />
-<meta name="description" content="A Azenha Guest House fica a apenas 5 min a pé da Lagoa e do Mar e a 10min dos restaurantes, bares, supermercados e outros serviços da Vila. O edifício, uma antiga azenha que servia de apoio à população para a moagem dos cereais, foi totalmente renovado e apresenta um toque de modernidade, embora mantendo a sua traça original. A Guest House oferece 4 quartos com terraço e uma sala comum com esplanada." />
-<!-- Google fonts -->
+<meta name="description" content="Azenha Guest House fica a 5 min a pé da Lagoa de Óbidos em Foz do Arelho. Turismo Rural, Quartos, Rooms, Caldas da Rainha, Leiria, Portugal - Telefone +351 96 58 68 037" />
 <link href='http://fonts.googleapis.com/css?family=Raleway:300,500,800|Old+Standard+TT' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Raleway:300,500,800' rel='stylesheet' type='text/css'>
-<!-- font awesome -->
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-<!-- bootstrap -->
 <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
-<!-- uniform -->
 <link type="text/css" rel="stylesheet" href="assets/uniform/css/uniform.default.min.css" />
-<!-- animate.css -->
 <link rel="stylesheet" href="assets/wow/animate.css" />
-<!-- gallery -->
 <link rel="stylesheet" href="assets/gallery/blueimp-gallery.min.css">
-<!-- favicon -->
 <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
 <link rel="icon" href="images/favicon.png" type="image/x-icon">
 <link rel="stylesheet" href="assets/style.css">
 <link rel="stylesheet" href="override.css">
 </head>
 <body id="home">
-<!-- top
-  <form class="navbar-form navbar-left newsletter" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Enter Your Email Id Here">
-        </div>
-        <button type="submit" class="btn btn-inverse">Subscribe</button>
-    </form>
- top -->
-<!-- header -->
 <nav class="navbar  navbar-default" role="navigation">
   <div class="container">
-    <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
+        <span class="sr-only">MENU</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
       <a class="navbar-brand" href="index.php"><img src="images/logo.png"  alt="holiday crown" style="max-width: 180px;"></a>
     </div>
-    <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li><a href="index.php" style="text-transform: uppercase;">Home </a></li>
@@ -57,7 +39,6 @@
         <li><a href="https://www.facebook.com/azenhaguesthouse/" target="_blank" style="text-transform: uppercase;">Facebook</a></li>
         <li><a href="https://www.instagram.com/azenhaguesthouse_turismorural/" target="_blank" style="text-transform: uppercase;">Instagram</a></li>
       </ul>
-    </div><!-- Wnavbar-collapse -->
-  </div><!-- container-fluid -->
+    </div>
+  </div>
 </nav>
-<!-- header -->
